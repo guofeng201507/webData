32d6bea34c0a441c6415d88a368589d92193984d
guo feng <guofeng20094@gmail.com>
2018-10-09 00:18:44 +0800

    Optimized code

--- dataService.py
+++ dataService.py
@@ -1,19 +1,20 @@
 import requests
 from lxml import etree
 
-s = requests.session()
+s = requests.Session()
 
 USERNAME = 'xxxxx'
-PASSWORD = 'xxxxx'
+PASSWORD = 'xxx'
 
-LOGIN_URL = 'https://securetrans.fitchratings.com/'
+INIT_URL = 'https://securetrans.fitchratings.com/'
 
+LOGIN_URL = 'https://securetrans.fitchratings.com/action/login'
+REDIRECT_URL = 'https://securetrans.fitchratings.com/action/storage'
 FILE_URL = 'https://securetrans.fitchratings.com/action/dload/data/20181001.bnk.hist.lc.zip'
 
+#Step 1: Open Page
 
-# req = requests.get(LOGIN_URL)
-req = s.get(LOGIN_URL)
-
+req = s.get(INIT_URL)
 login_html = etree.HTML(req.content)
 
 hidden_inputs = login_html.xpath(r'//form//input[@type="hidden"]')
@@ -24,18 +25,30 @@ form['pwd'] = PASSWORD
 
 print(form)
 
-
+#Step 2: login
 response = s.post(LOGIN_URL, data=form)
 
-print(response.text)
+# print(response.text)
+print(response.cookies)
+print(response.headers)
 
-# fileDownloaded = s.get(FILE_URL, data=form)
-# open('file.zip', 'wb').write(fileDownloaded.content)
-#
-# print(fileDownloaded.text)
-#
-# with open("downloaded.zip", "wb") as code:
-#     code.write(fileDownloaded.content)
+#Step 3: Redirect
+s.headers.update(response.headers)
+s.cookies.update(response.cookies)
+try:
+    nextResp = s.get(REDIRECT_URL)
+except Exception as e:
+    print(e)
+
+#Step 4: download
+fileDownloaded = s.get(FILE_URL, data=form)
+fileDownloaded = s.get(FILE_URL)
+open('file.zip', 'wb').write(fileDownloaded.content)
+
+print(fileDownloaded.text)
+
+with open("downloaded.zip", "wb") as code:
+    code.write(fileDownloaded.content)
 
 
 
